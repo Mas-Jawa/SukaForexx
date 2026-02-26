12de755d79345ddd5cc433b998279c6516114287
EvilRat <fendipendol124@gmail.com>
2026-02-26 13:55:43 +0700

    Add files via upload

---
+++ src/config/links.js
@@ -0,0 +1,38 @@
+// Konfigurasi Link untuk Website SukaForex
+// Edit link-link di sini sesuai kebutuhan
+
+export const BROKER_LINKS = [
+  {
+    name: 'Exness',
+    url: 'https://one.exnessonelink.com/a/f45n19j4pj?source=app', // GANTI DENGAN LINK IB EXNESS ANDA
+    color: 'from-blue-600 to-blue-800',
+    logo: 'EX'
+  },
+  {
+    name: 'HFM',
+    url: 'https://play.google.com/store/apps/details?id=com.hotforex.www.hotforex', // GANTI DENGAN LINK IB HFM ANDA
+    color: 'from-green-600 to-green-800',
+    logo: 'HF'
+  },
+  {
+    name: 'XM',
+    url: 'https://www.xmidbroker.org/referral?token=gMss6nBjTcx9ZpXIDeFp1w', // GANTI DENGAN LINK IB XM ANDA
+    color: 'from-red-600 to-red-800',
+    logo: 'XM'
+  },
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: 'Telegram',
+    url: 'https://t.me/sukaforex', // GANTI DENGAN LINK TELEGRAM ANDA
+    color: 'from-blue-400 to-blue-600',
+    icon: '📱'
+  },
+  {
+    name: 'WhatsApp',
+    url: 'https://wa.me/6281234567890', // GANTI DENGAN NOMOR WHATSAPP ANDA
+    color: 'from-green-400 to-green-600',
+    icon: '💬'
+  },
+];
